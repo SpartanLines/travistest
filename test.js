7d2f05d4abc09cb02f5006409e591b1105c23315
unknown <gamingconceited@gmail.com>
2019-04-30 12:40:07 +0200

    saved changes

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 var assert=require('assert')
 
 function isValidPassword(password){
-    if(password.length<=5 && password.length <=15)return true
+    if(password.length>=5 && password.length <=15)return true
     else return false
 }
 describe("TDD test suite",function(){
     describe("Password Verification",function(){
-        it("Tests Password length within range",function(){
+        it("Tests function length within range",function(){
             assert.equal(isValidPassword("abc123"),true)
             assert.equal(isValidPassword(""),false)
         })
